67a3f6eccc580af43219f66e721ba83766be5d93
Samay Singh Bisht <samay.samay987@gmail.com>
2022-06-26 02:08:24 +0200

    adding ami datasource configuration and filters

---
+++ 03-terraform-input-variables-and-datasources/terraform-manifests/c4-ami-datasource.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "amzlinux2" {
+  executable_users = ["self"]
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
