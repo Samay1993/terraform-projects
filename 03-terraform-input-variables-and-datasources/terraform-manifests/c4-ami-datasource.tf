9a3b875df9cf9ff88ceb2e80f4eb6a7d72530888
Samay Singh Bisht <samay.samay987@gmail.com>
2022-07-02 23:45:57 +0200

    Updating Data source configuration

--- 03-terraform-input-variables-and-datasources/terraform-manifests/c4-ami-datasource.tf
+++ 03-terraform-input-variables-and-datasources/terraform-manifests/c4-ami-datasource.tf
@@ -1,11 +1,12 @@
+# Get latest AMI id for Amazon Linux 2 OS
 data "aws_ami" "amzlinux2" {
-  executable_users = ["self"]
   most_recent      = true
   owners           = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-gp2"]
+    values = ["amzn2-ami-kernel-5.10-hvm-*-gp2"] #complete image name is 'amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-gp2' but using * means it is going to consider all the versions
+                                                 # and as defined in line 3, it will pick the recent image for us.
   }
 
   filter {
